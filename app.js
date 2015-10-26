e90a5f11eb97ad50f674ca8f476e7c4c399400b3
Michael Haeuslmann <haeuslmann@gmail.com>
2015-10-26 16:07:19 +0100

    Removed redundant templating engine and moved index.html into public.

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.set('views', 'private/views');
-app.set('view engine', 'ejs');
-
+// static files like html, js, css or images are served from here
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.render('index');
-});
+// load routes and inject our app
+require('./app/routes')(app);
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Fair Projects app listening at http://%s:%s', host, port);
 });
