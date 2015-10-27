18af55ae460d534c6d4b302a3a78640782578165
Michael Haeuslmann <haeuslmann@gmail.com>
2015-10-27 10:30:12 +0100

    Set up Grunt for css, js and watch.

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        // JS TASKS ================================================================
+        // check all js files for errors
+        jshint: {
+            all: ['public/js/**/*.js']
+        },
+
+        // take all the js files and minify them into app.min.js
+        uglify: {
+            build: {
+                files: {
+                    'public/dist/scripts.min.js': ['public/js/**/*.js']
+                }
+            }
+        },
+
+        // CSS TASKS ===============================================================
+        // process the less file to style.css
+        //less: {
+        //    build: {
+        //        files: {
+        //            'public/dist/css/style.css': 'public/src/css/style.less'
+        //        }
+        //    }
+        //},
+
+        // take the processed style.css file and minify
+        cssmin: {
+            build: {
+                files: {
+                    'public/dist/style.min.css': 'public/styles/style.css'
+                }
+            }
+        },
+
+        // COOL TASKS ==============================================================
+        // watch css and js files and process the above tasks
+        watch: {
+            css: {
+                files: ['public/styles/**/*.css'],
+                tasks: ['cssmin']
+            },
+            js: {
+                files: ['public/js/**/*.js'],
+                tasks: ['jshint', 'uglify']
+            }
+        },
+
+        // watch our node server for changes
+        nodemon: {
+            dev: {
+                script: 'app.js'
+            }
+        },
+
+        // run watch and nodemon at the same time
+        concurrent: {
+            options: {
+                logConcurrentOutput: true
+            },
+            tasks: ['nodemon', 'watch']
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    grunt.registerTask('default', ['cssmin', 'jshint', 'uglify', 'concurrent']);
+
+};
