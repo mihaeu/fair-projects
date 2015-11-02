ac8e0e4ad2f95e2ab738da833373dde42141e946
Markus Rodler <Markus.Rodler@live.de>
2015-11-02 15:27:15 +0100

    Grundlegendes Routing integriert

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-var app = angular.module('fairProjects', ['ngResource']);
+var app = angular.module('fairProjects', ['ngResource', 'ngRoute']);
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider.
+            when('/', {
+                template: 'test it works',
+                controller: 'SubjectController'
+            }).
+            when('/about', {
+                template: '<h1>TODO create country detail view</h1>',
+                controller: 'SubjectController'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+});
--- public/js/controllers/SubjectController.js
+++ public/js/controllers/SubjectController.js
@@ -1,23 +1,28 @@
-app.controller('SubjectController', ['$http','subjectService', function ($http, subjectService) {
-    var subjectController = this;
-    var newSubject = '';
+app.controller('SubjectController', ['$http', 'subjectService', '$scope',
+    function ($http, subjectService, $scope, $route, $routeParams, $location) {
+        $scope.$route = $route;
+        $scope.$location = $location;
+        $scope.$routeParams = $routeParams;
+        $scope.name = "SubjectController";
+        var subjectController = this;
+        var newSubject = '';
 
-    subjectController.subjects = subjectService.getAll();
+        subjectController.subjects = subjectService.getAll();
 
-    subjectController.delete = function (subject) {
+        subjectController.delete = function (subject) {
 
-        subjectService.delete(subject, function () {
-            subjectController.subjects = _.without(subjectController.subjects, subject);
-        });
-    };
-
-    subjectController.create = function (name) {
-        var data = {
-            'name': name
+            subjectService.delete(subject, function () {
+                subjectController.subjects = _.without(subjectController.subjects, subject);
+            });
         };
-        subjectService.create(data, function(data) {
-            subjectController.subjects.push(data);
-            subjectController.newSubjectName = '';
-        });
-    };
-}]);
+
+        subjectController.create = function (name) {
+            var data = {
+                'name': name
+            };
+            subjectService.create(data, function (data) {
+                subjectController.subjects.push(data);
+                subjectController.newSubjectName = '';
+            });
+        };
+    }]);
