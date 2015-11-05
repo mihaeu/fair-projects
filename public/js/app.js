07b53f70cfcb67bfa934997ba41f3521ad622b7a
Marinus Noichl <marinus.noichl@stud.fh-rosenheim.de>
2015-11-05 17:10:47 +0100

    Delete duplicate angular controller definition. Since ngRoute-Setup the controller definition with directives in the index.html was redundant. Deleting the directive gives aibility to show other controller with ng-view.

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,17 @@ app.config(function ($routeProvider, $locationProvider) {
     $routeProvider
             .when('/', {
                 templateUrl: 'views/subject/list.html',
-                controller: 'SubjectController'
+                controller: 'SubjectController',
+                controllerAs: 'fair'
             })
             .when('/subject/:id', {
                 templateUrl: 'views/subject/show.html',
-                controller: 'SubjectController'
+                controller: 'SubjectController',
+                controllerAs: 'fair'
+            })
+            .when('/subject/:id/projects', {
+                templateUrl: 'views/project/list.html',
+                controller: 'ProjectController'
             })
             .otherwise({
                 redirectTo: '/'
