711678b3f8695e32522d0a4cdad677293a85b3a4
Michael Haeuslmann <haeuslmann@gmail.com>
2015-11-07 13:48:33 +0100

    Tested ProjectShowController (100%).

---
+++ tests/unit/client/controllers/SubjectShowControllerSpec.js
@@ -0,0 +1,21 @@
+describe('SubjectShowController', function() {
+
+    beforeEach(module('fairProjects'));
+
+    var $controller;
+    beforeEach(inject(function(_$controller_) {
+        $controller = _$controller_;
+    }));
+
+    var mockSubjectService = {
+        get: function() {}
+    }
+
+    it('loads the requestes subject', function() {
+        spyOn(mockSubjectService, 'get').and.returnValue({id: 1, name: 'test subject'});
+        var controller = $controller('SubjectShowController', {
+            subjectService: mockSubjectService}
+        );
+        expect(controller.subject.name).toBe('test subject');
+    });
+});
