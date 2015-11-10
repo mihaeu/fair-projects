f073ef0051ea0706bf87fca4ab196553052bc2a9
Michael Haeuslmann <haeuslmann@gmail.com>
2015-11-10 10:40:17 +0100

    Fixed style issues.

--- private/models/Project.js
+++ private/models/Project.js
@@ -3,11 +3,10 @@ var Schema = mongoose.Schema;
 
 var projectSchema = new Schema({
   name: {type: String, default: '', required: true},
-  description: {type: String, default: ''}
+  description: {type: String, default: ''},
 });
 
 module.exports = {
   model: mongoose.model('Project', projectSchema),
-  schema: projectSchema
+  schema: projectSchema,
 };
-
