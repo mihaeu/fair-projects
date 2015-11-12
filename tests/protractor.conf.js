432261af282da7af0bf8b1c405a293e97c6b417d
Michael Haeuslmann <haeuslmann@gmail.com>
2015-11-12 22:37:13 +0100

    Changed protractor browser to firefox.

--- tests/protractor.conf.js
+++ tests/protractor.conf.js
@@ -2,4 +2,7 @@ exports.config = {
   framework: 'jasmine2',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['../tests/e2e/**/*.js'],
+  capabilities: {
+    browserName: 'firefox',
+  },
 };
