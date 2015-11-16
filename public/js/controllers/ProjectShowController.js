d6d3e0d66fe2eb25a845e8502617f4e359a5d99d
Fabian Gerbig <fabian.gerbig@gmail.com>
2015-11-16 15:42:02 +0100

    add missing functions ProjectShowController for participants

--- public/js/controllers/ProjectShowController.js
+++ public/js/controllers/ProjectShowController.js
@@ -1,5 +1,8 @@
 app.controller('ProjectShowController', ['$http', 'subjectService', 'projectService', 'participantService', '$routeParams',
   function($http, subjectService, projectService, participantService, $routeParams) {
+
+    'use strict';
+
     var _this = this;
 
     /**
@@ -16,7 +19,15 @@ app.controller('ProjectShowController', ['$http', 'subjectService', 'projectServ
         id: $routeParams.projectId,
       };
       _this.project = projectService.get(requestParameterProject);
+
+      var requestParameterParticipant = {
+        subject: $routeParams.subjectId,
+        project: $routeParams.projectId,
+      };
+      _this.participants = participantService.getAll(requestParameterParticipant);
     };
+
+    _this.init();
 
     /**
      * Updates the current project
@@ -33,6 +44,39 @@ app.controller('ProjectShowController', ['$http', 'subjectService', 'projectServ
       projectService.update(where, updateData);
     };
 
-    _this.init();
+    /**
+     * Creates a project within the current subject.
+     * Creates the project on the server and pushes it on success immediately to the project list.
+     *
+     * @param {Object} newProject
+     */
+    _this.createParticipant = function(newParticipant) {
+      var data = {
+        subject: _this.subject._id,
+        project: _this.project._id,
+        name: newParticipant.name,
+      };
+      participantService.create(data, function(data) {
+        _this.participants.push(data);
+        newParticipant.name = '';
+      });
+    };
+
+    /**
+     * Deletes a project.
+     *
+     * @param {Object} project
+     */
+    _this.deleteParticipant = function(participant) {
+      var requestParameterParticipant = {
+        subject: _this.subject._id,
+        project: _this.project._id,
+        id: participant._id,
+      };
+      participantService.delete(requestParameterParticipant)
+          .$promise.then(function() {
+            _this.participants = _.without(_this.participants, participant);
+          });
+    };
   },
 ]);
