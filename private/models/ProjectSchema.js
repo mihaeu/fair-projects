5188a22d086d658f66765c37846cfa89f05bdde0
FaGerbig <fabian.gerbig@gmail.com>
2015-11-16 15:44:10 +0100

    modify ProjectSchema for participants

--- private/models/ProjectSchema.js
+++ private/models/ProjectSchema.js
@@ -4,10 +4,12 @@ module.exports = (function() {
 
   var mongoose = require('mongoose');
   var Schema = mongoose.Schema;
+  var participantSchema = require('../models/ParticipantSchema.js');
 
   var projectSchema = new Schema({
     name: {type: String, default: '', required: true},
     description: {type: String, default: ''},
+    participants: [participantSchema],
   });
 
   return projectSchema;
