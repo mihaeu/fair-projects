589302c0f0d2c574717d42e96a5beb7b66f04ab8
Markus Rodler <Markus.Rodler@live.de>
2015-12-17 16:54:08 +0100

    Add Sortable Plugin

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('fairProjects', ['ngResource', 'ngRoute']);
+var app = angular.module('fairProjects', ['ngResource', 'ngRoute', 'ui.tree']);
 app.config(function($routeProvider) {
   $routeProvider
     .when('/', {
