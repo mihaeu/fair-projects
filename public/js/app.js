01254301e174dd7695e0f3f6fe12d04ec923531f
Markus Rodler <Markus.Rodler@live.de>
2015-12-17 16:08:34 +0100

    Remove WIP

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('fairProjects', ['ngResource', 'ngRoute', 'ui.tree']);
+var app = angular.module('fairProjects', ['ngResource', 'ngRoute']);
 app.config(function($routeProvider) {
   $routeProvider
     .when('/', {
