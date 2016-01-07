70bf7f0c3caaaf2de6251cf851513d2684a2f74b
Marinus Noichl <marinus.noichl@stud.fh-rosenheim.de>
2016-01-07 16:55:18 +0100

    Refactor Authentification

--- private/routers/AuthenticationRouter.js
+++ private/routers/AuthenticationRouter.js
@@ -1,91 +1,18 @@
+var express = require('express');
+var authenticationRouter = express.Router();
+var passport = require('passport');
+
+// Helper
+var ensureAuthentication = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.status(401).end();
+};
+
 module.exports = function(app) {
-
   'use strict';
-
-  var express = require('express');
-  var authenticationRouter = express.Router();
-
-  var passport = require('passport');
-  var LocalStrategy = require('passport-local').Strategy;
-
-  var userRepository = require('../models/UserRepository');
-
-  var bCrypt = require('bcrypt-nodejs');
-
-  // Login
-  passport.use(new LocalStrategy(
-    function(username, password, done) {
-      userRepository.getByUsername(username).then(function(user) {
-
-        if (!user) {
-          return done(null, false);
-        }
-
-        if (!isValidPassword(user, password)) {
-          return done(null, false);
-        }
-
-        return done(null, user);
-      }, function(err) {
-
-        return done(err, null);
-      });
-    }
-  ));
-
-  // SignUp
-  passport.use('signup', new LocalStrategy(
-    {passReqToCallback: true},
-    function(req, username, password, done) {
-      var createUser = function() {
-        userRepository.getByUsername(username).then(function(user) {
-          if (user) {
-            // user exists already
-            return done(null, false);
-          }
-
-          var newUser = userRepository.create({
-            username: username,
-            password: createHash(password),
-          });
-          newUser.save(function(err, user) {
-            if (err) {
-              // error on save
-              return done(err, null);
-            }
-
-            //success
-            return done(null, user);
-          });
-        });
-      };
-
-      process.nextTick(createUser);
-    }
-  ));
-
-  // Init Passport
-  var session = require('express-session');
-  var cookieParser = require('cookie-parser');
-  app.use(cookieParser());
-  app.use(session({secret: 'asd87a9d87a98d72d798wd9a7sd', saveUninitialized: true, resave: true}));
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  passport.serializeUser(function(user, done) {
-    done(null, user._id);
-  });
-
-  // User Handling
-  passport.deserializeUser(function(id, done) {
-    userRepository.getById(id)
-      .then(function(user) {
-        done(null, user);
-      }, function(err) {
-
-        done(err, null);
-      });
-  });
 
   // Routing
   authenticationRouter.post(
@@ -108,29 +35,12 @@ module.exports = function(app) {
     '/signup',
     passport.authenticate('signup'),
     function(req, res) {
-      return res.json();
+      return res.sendStatus(201);
     }
   );
 
-  // Helper
-  var isAuthenticated = function(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-
-    res.status(401).end();
-  };
-
-  var isValidPassword = function(user, password) {
-    return bCrypt.compareSync(password, user.password);
-  };
-
-  var createHash = function(password) {
-    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
-  };
-
   // require authentication for all API methods
-  authenticationRouter.use('/', isAuthenticated);
+  authenticationRouter.use('/', ensureAuthentication);
 
   return authenticationRouter;
 };
