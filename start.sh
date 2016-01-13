ba7e4243ebd308127dd6e5246e5cbf796e8f76d7
Michael Haeuslmann <haeuslmann@gmail.com>
2016-01-13 12:53:56 +0100

    Empty project for doc

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nohup sh -c mongod & 
-node /src/server.js
--- tests/protractor.conf.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  framework: 'jasmine2',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['../tests/e2e/**/*.js'],
-  capabilities: {
-    browserName: 'firefox',
-  },
-};
